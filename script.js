3ffb62e8cb871677354fd738ffc91c498d379b68
liaasilvia <105857437+liaasilvia@users.noreply.github.com>
2022-07-04 12:06:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+//scrolltop
+
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
+//cookie
+function hideCookie() {
+  let cookie = document.querySelector("#innercookie");
+  cookie.style = "display:none;";
+}
+function showCookie() {
+  let cookie = document.querySelector("#innercookie");
+  cookie.style = "display:block;";
+}
 
